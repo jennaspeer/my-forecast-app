f6f1456a29c4d849d0b2b70afba6ad2e5ed7bf7f
jennaspeer <jenspeer301@hotmail.com>
2024-07-21 16:25:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -54,3 +54,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
